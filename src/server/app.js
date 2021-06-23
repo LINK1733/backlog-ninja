0d9ac271150cd73893d08c4875d463d23adb5b58
LINK1733 <link1733@gmail.com>
2021-06-22 22:15:35 -0500

    Setup user accounts, mongoose, splash, session
    Setup user account creation and login. Configured sessions and mongoDB. Changed main page to be a login splash page if you aren't logged in and it will show the original home page once you are logged in. Got a nav bar setup with a logout option. Setup routes and controllers for registration and login. Created a form validation and catchAsync middleware. Added another webpack for the splash page that contains the form validation.

--- src/server/app.js
+++ src/server/app.js
@@ -1,17 +1,99 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const express = require('express')
-const ejs = require('ejs')
 const path = require('path')
+const mongoose = require('mongoose')
+const ejsMate = require('ejs-mate')
+const session = require('express-session')
+const passport = require('passport');
+const LocalStrategy = require('passport-local')
+const User = require('../server/models/user')
+const MongoStore = require('connect-mongo')
+const userRoutes = require('../server/routes/users')
+const flash = require('connect-flash');
+
+const dbUrl = 'mongodb://localhost:27017/game-tracker'
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('./public'))
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
+
+const secret = process.env.SESSION_SECRET;
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret
+    }
+});
+
+store.on("error", function (e) {
+    console.log("Session Store Error", e)
+})
+
+const sessionConfig = {
+    store,
+    name: 'GameTrackerSession',
+    secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        // secure: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+app.use('/', userRoutes);
+
 app.get('/', (req, res) => {
-    res.render('home')
+    if(req.user){
+        res.render('home')
+    } else {
+        res.render('splash')
+    }
 })
 
 app.listen(port, () => {
--- src/static/main.js
+++ src/static/main.js
@@ -1,11 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+} from "react-router-dom";
 
-function App() {
-    return <h1 className='text-center'>Hello from React</h1>
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <main className='container mt-5'>
+                    <h1 className='text-center'>Hello from React</h1>
+            </main>
+        )
+    }
 }
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+        
+    render() {
 
+        return (
+            <Router>
+                <div>
+                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+                        <ul className="container-fluid m-auto">
+                            <Link to='/' className="navbar-brand">Home</Link>
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Account</a>
+                                    <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
+                                        <li className="nav-item"><a className="nav-link link-light" href="/logout">Logout</a></li>
+                                    </ul>
+                                </li>
+                                </ul>
+                            </div>
+                        </ul>
+                    </nav>
+
+                    <Switch>
+                        <Route path='/'>
+                            <Home />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
+}
+// ms-auto mt-2 btn btn-primary btn-small
 ReactDom.render(
-    <App />,
+        <App />,
     document.querySelector('#app-root')
 )
