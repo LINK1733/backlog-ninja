8b4c7bff86e8ce25b9759a455c5517499fa2ab33
LINK1733 <link1733@gmail.com>
2022-03-10 07:39:23 -0600

    Remove right margin to remove overflow
    I had missed that when I removed the m-0 that was here it made the margins overflow a little bit. Added me-0 so it would remove the right margin and no longer overflow.

--- src/static/components/gameListKeeper.js
+++ src/static/components/gameListKeeper.js
@@ -35,7 +35,7 @@ export default function GameListKeeper({
 
 	return (
 		<div>
-			<Row className="gy-3 pb-3 justify-content-center">
+			<Row className="gy-3 pb-3 me-0 justify-content-center">
 				<Form className="align-items-center" onSubmit={handleSubmit}>
 					<Form.Group className="w-25 py-2 mx-auto rounded px-2 ">
 						<Form.Control
